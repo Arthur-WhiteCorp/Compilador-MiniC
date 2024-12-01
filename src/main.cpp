645b4e74ff648313472a03d662a211e35ad6836c
Arthur <arthursilvamatias@gmail.com>
2024-12-01 17:26:15 -0400

    cmake

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
+//
+//  main.cpp
+//  antlr4-cpp-demo
+//
+//  Created by Mike Lischke on 13.03.16.
+//
+
+#include <iostream>
+
+#include "antlr4-runtime.h"
+#include "TLexer.h"
+#include "TParser.h"
+
+//using namespace antlrcpptest;
+using namespace antlr4;
+
+int main(int , const char **) {
+  std::cout << "Um pequeno passo pra o tcc um grande passo para um anão" << std::endl;
+
+  return 0;
+}
---
+++ src/parser_lexer/TParserBaseListener.cpp
@@ -0,0 +1,7 @@
+
+// Generated from /home/arthur/git/Compilador-MiniC-8-Bits/grammar/TParser.g4 by ANTLR 4.13.2
+
+
+#include "TParserBaseListener.h"
+
+
